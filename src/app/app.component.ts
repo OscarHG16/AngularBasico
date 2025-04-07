abfbbcb0fbacf3975c426ae6ed82d2ccb3faad6a
OscarHG16 <oscarpacon321@gmail.com>
2025-04-07 10:36:33 -0600

    Segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'; //este nos permite definir un componente
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', //Nombre que se le dara a nuestra app
+  standalone: true, //Indica que este componente es independiente y no depende de otros componentes
+  imports: [FormsModule], //Importamos el modulo de formularios para poder usar ngModel
+  templateUrl: './app.component.html', //Ruta del template html
+  styleUrls: ['./app.component.css'] //Ruta del css
 })
+
+//Toda la logica de la app se encuentra en este componente
 export class AppComponent {
-  title = 'mi-app-angular';
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  convertirCelsius() {
+    this.celsius = (this.fahrenheit - 32) * 5 / 9;
+  }
+
+  convertirFahrenheit() {
+    this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+/**
+ Este archivo configura la aplicación en Angular, estableciendo los proveedores (providers)
+ necesarios para el funcionamiento de la app.
+ */
+
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'; // nos ayuda a definir las rutas de la aplicación
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
+/*
+Los providers son los proovedores de la aplicación,
+que permiten inyectar dependencias en los componentes y servicios.
+para que funcione bien la aplicación.
+*/
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), importProvidersFrom(FormsModule),
+    ]
 };
